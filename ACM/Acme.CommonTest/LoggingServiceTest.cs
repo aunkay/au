2538ac9637644115ba63fe4ff735f57700cb076e
alperu <alperu@microsoft.com>
2018-01-17 09:56:33 +0300

    1st commit

---
+++ ACM/Acme.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Common;
+using ACM.BL;
+using System.Collections.Generic;
+
+namespace Acme.CommonTest
+{
+    [TestClass]
+    public class LoggingServiceTest
+    {
+        [TestMethod]
+        public void WriteToFileTest()
+        {
+           
+
+            // Assert
+            // Nothing here to assert
+        }
+    }
+}
